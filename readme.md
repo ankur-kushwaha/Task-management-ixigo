9d553883d31fa139f5c12e26a4cfc47a96ba0ca5
Ankur Kushwaha <ankur.kus1@gmail.com>
2017-03-28 12:58:00 +0530

    base commit

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Task Management Application
+
+## Steps to run application
+1. Clone the repository
+2. `bower install`
+3. `npm install`
+3. `npm start`
---
+++ script.js
@@ -0,0 +1,186 @@
+(function () {
+    'use strict';
+    var listCount = 0;
+    var data = {}
+
+
+    var db;
+    var request = window.indexedDB.open("newDatabase", 1);
+
+    request.onerror = function (event) {
+        console.log("error: ");
+    };
+
+    request.onsuccess = function (event) {
+        db = request.result;
+        //read();
+        console.log("success: " + db);
+    };
+    $(function () {
+        var listId = createList("test");
+        createCard(listId, "testCard", "Ankur", ["tag1", "tag2"]);
+        listId = createList("test2");
+        console.log(data);
+        render();
+
+
+        // read();
+    });
+
+    function createList(name) {
+        var newList = {
+            name: name,
+            cards: {},
+            cardCounter: 0
+        };
+        data[++listCount] = newList;
+        return listCount;
+    }
+
+    function createCard(listId, desc, user, tags) {
+        var list = data[listId];
+        var newCard = {
+            desc: desc,
+            user: user,
+            tags: tags
+        };
+        list.cards[++list.cardCounter] = newCard;
+        return list.cardCounter;
+    }
+
+    function deleteList(id) {
+        delete data[id];
+    }
+
+    function editCard() {
+
+    }
+
+    function render() {
+
+        var taskContainer = $('#taskContainer');
+        taskContainer.html("");
+        var x = "test";
+
+        for (var listId in data) {
+            var list = data[listId];
+            var listTemplate =
+                `
+                <div class="col-lg-3" id="list_${listId}">
+                <div class="panel panel-default">
+                    <div class="panel-heading">${list.name}
+                    <button onclick="onDeleteList(${listId})">Del</button>
+                    </div>
+                    <div class="panel-body">
+                        <input type="text" placeholder="description" class="cardName">
+                        <input type="text" class="user" placeholder="UserName" >
+                        <input type="text" placeholder="Tags"  class="tags">
+                        <button onclick="onClickAddNewCard(${listId})">Add Card</button>
+                    </div>
+                
+                </div>
+            </div>`;
+            taskContainer.append(listTemplate);
+
+            renderCard(listId);
+
+
+        }
+    }
+
+
+
+    function renderCard(listId) {
+        var list = data[listId];
+        for (var cardId in list.cards) {
+            var card = list.cards[cardId];
+            var tags = card.tags && card.tags.join(",");
+            var cardTemplate = `
+                <div class="card" draggable="true" ondragstart="drag(event,${listId},${cardId})" ondrop="drop(event,${listId})" ondragover="allowDrop(event)">
+                    <div class="description">
+                        ${card.desc}
+                    </div>
+                    <span class="pull-right tags">${tags}</span>
+                    <span class="username">${card.user}</span>
+                </div>
+                `;
+            $("#list_" + listId).find('.panel-body').append(cardTemplate);
+        }
+    }
+
+    function add() {
+        var request = db.transaction(["task"], "readwrite")
+            .objectStore("task")
+            .add({
+                id: "00",
+                data: data
+            });
+
+        request.onsuccess = function (event) {
+            db = request.result;
+            console.log("Data saved successfully");
+        };
+
+        request.onerror = function (event) {
+            console.log("failed to save data");
+        }
+    }
+
+    function read() {
+        var transaction = db.transaction(["task"]);
+        var objectStore = transaction.objectStore("task");
+        var request = objectStore.get("00");
+
+        request.onerror = function (event) {
+            console.log("Unable to retrieve data from database!");
+        };
+
+        request.onsuccess = function (event) {
+            // Do something with the request.result!
+            if (request.result) {
+                data = request.result.data;
+            } else {
+                console.log("Kenny couldn't be found in your database!");
+            }
+            render();
+        };
+    }
+
+    window.addNewList = function () {
+        console.log(arguments)
+        var name = $('#listName').val();
+        createList(name);
+        render();
+        $('#listName').val("");
+    };
+    window.onDeleteList = function (id) {
+        deleteList(id);
+        render();
+    };
+
+    window.onClickAddNewCard = function (listId) {
+        var list = $("#list_" + listId);
+        var desc = list.find('.cardName').val()
+        var name = list.find('.user').val()
+        var tags = list.find('.tags').val().split(",");
+        createCard(listId, desc, name, tags);
+        render();
+    };
+
+    window.drag = function (ev, listId) {
+        ev.dataTransfer.setData("listId", listId);
+        ev.dataTransfer.setData("cardId", cardId);
+    };
+
+    window.allowDrop = function allowDrop(ev) {
+        ev.preventDefault();
+    }
+
+    window.drop = function drop(ev) {
+        ev.preventDefault();
+        var sourceList = ev.dataTransfer.getData("listId");
+        var cardId = ev.dataTransfer.getData("cardId");
+
+    };
+
+}(window));
